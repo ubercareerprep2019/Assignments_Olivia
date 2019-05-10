f62eb34098bd148f83af21130a23e7d8de1a85c1
Olivia Lee <olivia.suji.lee@berkeley.edu>
2019-05-10 01:32:03 -0700

    updates to hw1 part2

---
+++ hw1/Permutation.java
@@ -0,0 +1,54 @@
+/* Function takes two strings and returns true if 
+ * one string is a permutation of the other, false otherwise
+ *  
+ * @author Olivia Lee
+ */
+
+public class Permutation {
+
+    public static boolean isStringPermutation(String s1, String s2) {
+        //base case:
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        boolean forward = true; //initial check for forward permutation
+        char front = s1.charAt(0);
+        int s2Index = s2.indexOf(front); //searching for first character of s1 in s2
+        
+        if (start == -1) {
+            return false;
+        }
+        
+        //forward case
+        while (s2Index != -1) {
+            String sub = s2.substring(s2Index);
+            String corrS1 = s1.substring(0, sub.length()); //corresponding substring in s1
+            if (sub.equals(corrS1)) {
+                if (s2.substring(0, s2.length() - sub.length())) {
+                    return true;
+                }
+                s2Index = s2.substring(1).indexOf(front);
+            } 
+        }
+
+        //backward case: need to check one char at a time
+        int s1Index = 0;
+        int count = 1, complete = 0;
+        s2Index = s2.indexOf(front);
+        while (s2Index != -1) {
+            if (complete == s1.length()) {
+                return true;
+            }
+            if (s1.charAt(s1index) != s2.charAt(s2Index)) {
+                s2Index = s2.substring(count).indexOf(front);
+                count++;
+                complete = 0;
+                continue;
+            }
+            s1Index = (s1Index - 1) % s1.length();
+            s2Index = (s2Index - 1) % s2.length();
+            complete++;
+        }
+        return false;
+    }
+}
---
+++ hw1/permutationTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class permutationTest {
+
+    /** Performs a few arbitrary tests to check if isStringPermutation method is correct */
+
+    @Test 
+    public void testSimple() {
+        String s1 = "hello";
+        String s2 = "elloh";
+        assertTrue(Permutation.isStringPermutation(s1, s2));
+    }
+
+    @Test 
+    public void testEmpty() {
+        String s1 = "hello";
+        String s2 = "";
+        assertFalse(Permutation.isStringPermutation(s1, s2));
+    }
+}
